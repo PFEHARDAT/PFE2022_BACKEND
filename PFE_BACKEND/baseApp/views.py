4f36a2ff28bb1ccb075a00453d2f9e899252f06b
hhardat <hugohardat@gmail.com>
2022-12-06 15:44:49 +0100

    first route tuto

--- PFE_BACKEND/baseApp/views.py
+++ PFE_BACKEND/baseApp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from PFE_BACKEND.baseApp import models
+from serializers import *
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view
 
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def user_list(request):
+    if request.method == 'GET':
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
