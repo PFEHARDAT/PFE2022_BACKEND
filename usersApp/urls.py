ddf841deae51d1787ff6ebba9de4d9cd65f2dd94
Hugo <hugohardat@gmail.com>
2022-12-09 15:59:21 +0100

    added endpoint  on user details

--- usersApp/urls.py
+++ usersApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import UserListAPIView
+from .views import (
+    UserListAPIView,
+    UserDetailAPIView
+    )
 
 
 urlpatterns = [
     path('api', UserListAPIView.as_view()),
+    path('api/<int:pk>', UserDetailAPIView.as_view())
 ]
--- usersApp/views.py
+++ usersApp/views.py
@@ -19,3 +19,28 @@ class UserListAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class UserDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
