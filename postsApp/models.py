5533c51b20223ebcaeebe7ad78c079888a21e541
CedricNiyiVinci <cedric.niyikiza@student.vinci.be>
2022-12-10 19:56:46 +0100

    refactor -  create model (1 field missed)

--- postsApp/models.py
+++ postsApp/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 
+class Post(models.Model):
+    content = models.CharField(max_length= 300)
+    publication_date = models.DateTimeField(auto_now_add = True, auto_now = False) 
+    like_count = models.BigIntegerField()
+    comment_count = models.BigIntegerField() 
 
