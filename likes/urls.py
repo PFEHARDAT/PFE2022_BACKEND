7352e3fc19da2e2ad1e22880740a6d5c0ab6d623
Hugo <hugohardat@gmail.com>
2022-12-11 13:37:11 +0100

    small update but saved this time

--- likes/urls.py
+++ likes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import LikesView
+from .views import LikesView, LikedPostView, LikesOnPostView
+
 
 urlsPatterns = [
     path('like/', LikesView.as_view()),
-    path('liked_list/<int:user_id>', LikesView.as_view()),
-    path('like_list/<int:post_id>', LikesView.as_view())
+    path('liked_list/<int:user_id>', LikedPostView.as_view()),
+    path('like_list/<int:post_id>', LikesOnPostView.as_view())
 ]
