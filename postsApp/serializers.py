1bda3f9650837dafed9e780f1cd4c05c64159a89
CedricNiyiVinci <cedric.niyikiza@student.vinci.be>
2022-12-11 15:24:41 +0100

    refactor - add post id

--- postsApp/serializers.py
+++ postsApp/serializers.py
@@ -4,4 +4,4 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["user_id", "content", "publication_date", "like_count", "comment_count", "retweet_count"]
+        fields = ["id","user_id", "content", "publication_date", "like_count", "comment_count", "retweet_count"]
