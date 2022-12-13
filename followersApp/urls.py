e4d89a675e616d6400f181681a9c178134ce9864
AbdenourDidi <abdenour.didi@student.vinci.be>
2022-12-13 18:34:12 +0100

    refactor followersApp subscriptionsApp

--- followersApp/urls.py
+++ followersApp/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("all/<int:user>", views.AllFollowerView.as_view(), name="follower_all"),
-    path("create/", views.FollowerToUserView.as_view(), name="follower_create")
+    path("all/<int:user>", views.FollowerView.as_view(), name="follower_all"),
+    path("create/", views.FollowerView.as_view(), name="follower_create"),
+    path("delete/", views.FollowerView.as_view(), name="follower_delete")
 ]
--- subscriptionsApp/views.py
+++ subscriptionsApp/views.py
@@ -11,17 +11,15 @@ from usersApp.models import User
 
 # Create your views here.
 
-class AllSubscriptionView(APIView):
+class SubscriptionView(APIView):
+    serializer_class = SubscribeToUserSerializer
     def get(self, request:Request, user):
         subscriptions = Subscription.objects.all().filter(user=user)
         serializer = SubscribeToUserSerializer(subscriptions, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-class SubscribeToUserView(APIView):
-    serializer_class = SubscribeToUserSerializer
     def post(self, request:Request):
         user = request.data.get("user")
-        subscription = request.data.get("subscription")
         data = request.data
         serializer = self.serializer_class(data=data)
         if serializer.is_valid():
@@ -29,6 +27,13 @@ class SubscribeToUserView(APIView):
             self.updateSubscriptionsCount(user, True)
             response = {"message": "Subscription Created Successfully", "data": serializer.data}
             return Response(data=response, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+    def delete(self, request:Request):
+        user = request.data.get("user")
+        subscription = request.data.get("subscription")
         subscription_exist = Subscription.objects.filter(user=user, subscription=subscription)
         subscription_exist.delete()
         self.updateSubscriptionsCount(user, False)
