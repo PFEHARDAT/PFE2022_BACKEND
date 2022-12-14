e89eb45caf8bb83982c987e89afd38dece74a25a
CedricNiyiVinci <cedric.niyikiza@student.vinci.be>
2022-12-14 19:58:01 +0100

    push changes

--- subscriptionsApp/views.py
+++ subscriptionsApp/views.py
@@ -8,7 +8,7 @@ from .serializers import SubscribeToUserSerializer
 from .models import Subscription
 from usersApp.models import User
 from followersApp.models import Follower
-
+from followersApp.views import FollowerView
 
 # Create your views here.
 
@@ -22,20 +22,32 @@ class SubscriptionView(APIView):
     def post(self, request:Request):
         user = request.data.get("user")
         subscription = request.data.get("subscription")
+        print(request.data)
         data = request.data
+        print("----")
+        print("----")
+
         serializer = self.serializer_class(data=data)
+        print(serializer)
+        # print("----")
+
+        # print(serializer.data)
+        print("----")
+        print("----")
+        
         if serializer.is_valid():
+            print("==>  rentré dans if")
             serializer.save()
             self.updateSubscriptionsCount(user, True)
-            self.updateFollowerCount(subscription, True)
-            self.addToFollower(subscription, user)
+            url("followers/create/", )
             response = {"message": "Subscription Created Successfully", "data": serializer.data}
             return Response(data=response, status=status.HTTP_201_CREATED)
-        subscription_exist = Subscription.objects.filter(user=user, subscription=subscription)
-        subscription_exist.delete()
-        self.updateSubscriptionsCount(user, False)
-        self.updateFollowerCount(subscription_exist, False)
-        self.deleteFromFollower(subscription_exist, user)
+        # subscription_exist = Subscription.objects.filter(user=user, subscription=subscription)
+        # subscription_exist.delete()
+        # self.updateSubscriptionsCount(user, False)
+        # self.updateFollowerCount(subscription_exist, False)
+        # self.deleteFromFollower(subscription_exist, user)
+        print("hello world")
         return Response(data='DELETED', status=status.HTTP_200_OK)       
 
 
@@ -50,19 +62,26 @@ class SubscriptionView(APIView):
     
     def updateSubscriptionsCount(self, user, increment):
         user = User.objects.get(id=user)
-        if increment :
-            user.following_count += 1
-        else:
-            user.following_count -= 1
-        user.save()
+        print("user that make the action => ", user)
+        # if increment :
+        #     user.following_count += 1
+        # else:
+        #     user.following_count -= 1
+        # user.save()
     def updateFollowerCount(self, user, increment):
         user = User.objects.get(id=user)
-        if increment :
-            user.followers_count += 1
-        else:
-            user.followers_count -= 1
-        user.save()
+        print("followed user=> ", user)
+        # if increment :
+        #     user.followers_count += 1
+        # else:
+        #     user.followers_count -= 1
+        # user.save()
     def addToFollower(self, user, follower):
+        
+        print(">>> In addToFollower(self, user, follower):")
+        print("- user:", user.id)
+        print("- follower:", follower.id)    
+
         Follower.objects.create(user=user, follower=follower)
     def deleteFromFollower(self, user, follower):
         follower = Follower.objects.filter(user_id=user, follower=follower)
