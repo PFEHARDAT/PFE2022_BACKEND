57701a4527ac35e2356c5af493c6487d28f06df0
Hugo <hugohardat@gmail.com>
2022-12-14 22:25:31 +0100

    deleted post on followers

--- followersApp/urls.py
+++ followersApp/urls.py
@@ -6,6 +6,4 @@ from . import views
 
 urlpatterns = [
     path("all/<int:user>", views.FollowerView.as_view(), name="follower_all"),
-    path("create/", views.FollowerView.as_view(), name="follower_create"),
-    ###path("delete/", views.FollowerView.as_view(), name="follower_delete")
 ]
--- followersApp/views.py
+++ followersApp/views.py
@@ -17,37 +17,3 @@ class FollowerView(APIView):
         followers = Follower.objects.all().filter(user=user)
         serializer = FollowerToUserSerializer(followers, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request:Request):
-        user = request.data.get("user")
-        follower = request.data.get("follower")
-        data = request.data
-        serializer = self.serializer_class(data=data)
-        follower = Follower.objects.filter(user=user, follower=follower)
-        if follower.exists():
-            follower.delete()
-            self.updateFollowerCount(user, False)
-            return Response(data='DELETED', status=status.HTTP_200_OK)
-        elif serializer.is_valid():
-            serializer.save()
-            self.updateFollowerCount(user, True)
-            response = {"message": "Follower Created Successfully", "data": serializer.data}
-            return Response(data=response, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    
-    #def delete(self, request:Request):
-    #    user = request.data.get("user")
-    #    follower = request.data.get("follower")
-    #    follower_exist = Follower.objects.filter(user=user, follower=follower)
-    #    follower_exist.delete()
-    #    self.updateFollowerCount(user, False)
-    #    return Response(data='DELETED', status=status.HTTP_200_OK)
-    
-    def updateFollowerCount(self, user, increment):
-        user = User.objects.get(id=user)
-        if increment:
-            user.followers_count += 1
-        else:
-            user.followers_count -= 1
-        user.save()
