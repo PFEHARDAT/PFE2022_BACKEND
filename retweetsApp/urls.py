10dcd4b258c8a7ba9e97d1786dff1ca403856440
AbdenourDidi <abdenour.didi@student.vinci.be>
2022-12-14 22:06:22 +0100

    add feature post -- retweet and like by user

--- retweetsApp/urls.py
+++ retweetsApp/urls.py
@@ -2,8 +2,9 @@
 from django.urls import path
 from .views import RetweetAPIView, RetweetListAPIView
 
-urlpatterns =[
+urlpatterns = [
     path("create/", RetweetAPIView.as_view(), name="retweet_create"),
-    path("delete/", RetweetAPIView.as_view(), name="retweet_delete"),
-    path("all/<int:user>", RetweetListAPIView.as_view(), name="retweet_all")
+    ###path("delete/", RetweetAPIView.as_view(), name="retweet_delete"),
+    path("all/<int:user>", RetweetListAPIView.as_view(), name="retweet_all"),
+    path("exist/", RetweetAPIView.as_view(), name="retweet_exist")
 ]
