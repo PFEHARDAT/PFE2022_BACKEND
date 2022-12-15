d7367e21d0d86223b9944185eb42f6781549a223
Hugo <hugohardat@gmail.com>
2022-12-15 17:36:16 +0100

    url fix

--- postsApp/urls.py
+++ postsApp/urls.py
@@ -12,6 +12,6 @@ urlpatterns =[
     path('', PostListAPIView.as_view()),
     path('<int:post_id>', PostDetailsAPIView.as_view()),
     path('user/<int:user_id>', PostByUserAPIView.as_view()),
-    path('comments/<int:post_id>', CommentsListAPIView.as_view()),
+    path('comments', CommentsListAPIView.as_view()),
     path('subscription/<int:user_id>', PostFromSubscriptionAPIView.as_view())
 ]
