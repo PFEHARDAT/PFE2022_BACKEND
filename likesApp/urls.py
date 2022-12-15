aa6d6f633e0ea3ac7ab2c18e46299b9544317ace
hhardat <hugohardat@gmail.com>
2022-12-15 11:20:39 +0100

    delete on like is back

--- likesApp/urls.py
+++ likesApp/urls.py
@@ -4,8 +4,7 @@ from .views import LikesView, LikedPostView, LikesOnPostView
 
 urlpatterns = [
     path('', LikesView.as_view()),
-    path('liked_list/<int:user_id>', LikedPostView.as_view()),
-    path('like_list/<int:post_id>', LikesOnPostView.as_view()),
-    path("exist/<int:user>/<int:post>", LikesView.as_view(), name="like_exist")
-
+    path('/liked_list/<int:user_id>', LikedPostView.as_view()),
+    path('/like_list/<int:post_id>', LikesOnPostView.as_view()),
+    path("/exist/<int:user>/<int:post>", LikesView.as_view(), name="like_exist")
 ]
--- likesApp/views.py
+++ likesApp/views.py
@@ -14,36 +14,30 @@ class LikesView(APIView):
             'user': request.data.get('user'),
             'post': request.data.get('post')
         }
-        like = Like.objects.filter(user=data['user'], post=data['post'])
         serializer = self.serializer_class(data=data)
-        print(serializer.is_valid())
-        if like.exists():
-            like.delete()
-            self.updateCount(data['post'], False)
-            return Response({'message': 'DELETED'}, status=status.HTTP_200_OK)
-        elif serializer.is_valid():
+        if serializer.is_valid():
             serializer.save()
             self.updateCount(data['post'], True)
             return Response({'message': 'CREATED', 'data': serializer.data}, status=status.HTTP_201_CREATED)
-    
         return Response({'message': 'Like already exists'}, status=status.HTTP_409_CONFLICT)
 
-    def get(self, request, user, post):
+    def get(self, request):
+        user = request.query_params.get('user')
+        post = request.query_params.get('post')
         if Like.objects.filter(user=user, post=post).exists():
             return Response(data= "True", status=status.HTTP_200_OK)
         return Response(data= "False", status=status.HTTP_200_OK)
         
-    #def delete(self, request):
-    #    data = {
-    #        'user': request.data.get('user'),
-    #        'post': request.data.get('post')
-    #    }
-    #    like = Like.objects.filter(user=data['user'], post=data['post'])
-    #    if not like.exists():
-    #        return Response({'message': 'Like does not exist'}, status=status.HTTP_409_CONFLICT)
-    #    like.delete()
-    #    self.updateCount(data['post'], False)
-    #    return Response({'message': 'DELETED'}, status=status.HTTP_200_OK)
+    def delete(self, request):
+        user = request.query_params.get('user')
+        post = request.query_params.get('post')
+        like = Like.objects.filter(user=user, post=post)
+        if not like.exists():
+            return Response({'message': 'Like does not exist'}, status=status.HTTP_409_CONFLICT)
+        like.delete()
+        self.updateCount(post, False)
+        return Response({'message': 'DELETED'}, status=status.HTTP_200_OK)
+
     def updateCount(self, post, increment):
         post = Post.objects.get(id=post)
         if increment:
@@ -51,6 +45,7 @@ class LikesView(APIView):
         else:
             post.like_count -= 1
         post.save()
+
 class LikedPostView(APIView):   
     def get(self, request, user_id):
         user = User.objects.get(id=user_id)
@@ -70,7 +65,3 @@ class LikesOnPostView(APIView):
             like_list.append(like)
         serializer = LikeUserSerializer(likes, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-class CSRFTokenView(APIView):
-    def get(self, request):
-        return Response(data={'csrfToken': request.META['CSRF_COOKIE']}, status=status.HTTP_200_OK)
